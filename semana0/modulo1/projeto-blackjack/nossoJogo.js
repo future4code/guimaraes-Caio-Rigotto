a91bddb49aadda71cb6dedd0a45dc7cdbc5cf74c
Caio Rigotto <caiorigotto@gmail.com>
2021-12-01 12:03:59 -0300

    Projeto completo sem desafio

--- semana0/modulo1/projeto-blackjack/nossoJogo.js
+++ semana0/modulo1/projeto-blackjack/nossoJogo.js
@@ -11,11 +11,45 @@
  * 
  */
 {
-   console.log("Boas vindas ao jogo de Blackjack!")
-   if(confirm("Quero começar a jogar?")){
-      const cartaJogador = comprarCarta()
-      console.log(`Carta: ${cartaJogador.texto} Valor: ${cartaJogador.valor}`)
-   }else{
-      console.log("O jogo acabou!")
+   if(confirm("Quer começar a jogar:")){
+      console.log("Boas vindas ao jogo de Blackjack!")
+      let comprarJogador = []
+      for(let i=0;i<2;i++){
+         comprarJogador.push(comprarCarta())
+      }
+      // console.log(comprarJogador)
+      const valorJogador = comprarJogador.map((comprarJogador)=>{
+         return comprarJogador.valor}) 
+      const cartasJogador = comprarJogador.map((comprarJogador)=>{
+         return comprarJogador.texto})
+         const valorTotalJog = Number(valorJogador[0]+valorJogador[1])
+         console.log('Usuário - Cartas:',cartasJogador[0], cartasJogador[1], `Valor: ${valorTotalJog}`)
+
+         let comprarComputador = []
+         for(let i=0;i<2;i++){
+            comprarComputador.push(comprarCarta())
+         }
+      // console.log(comprarComputador)
+         const valorComputador = comprarComputador.map((comprarComputador)=>{
+            return comprarComputador.valor}) 
+         const cartasComputador = comprarComputador.map((comprarComputador)=>{
+            return comprarComputador.texto})
+            const valorTotalComp = Number(valorComputador[0]+valorComputador[1])
+            console.log('Computador - Cartas:',cartasComputador[0], cartasComputador[1], `Valor: ${valorTotalComp}`)
+         if(valorTotalComp > valorTotalJog){
+            console.log("O Computador venceu!!")
+         }else
+         if(valorTotalJog > valorTotalComp){
+            console.log("O Jogador venceu!!")
+         }else
+         if(valorTotalComp === valorTotalJog){
+            console.log("Houve um empate!!")
+         }
+   }
+   else{
+   console.log("O jogo acabou!")
    }
 }
+
+// criar loop jog e comp compram 2 cartas - add as cartas em um array - imprimir respectivas cartas e valores
+// o jogador que tiver o maior valor ganha - valores iguais, empate
