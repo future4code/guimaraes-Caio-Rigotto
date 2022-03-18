a0d75e5514864f852ade91c5211bf8e510f1545f
Caio Rigotto <caiorigotto@gmail.com>
2022-03-17 21:44:42 -0300

    Começo do projeto

---
+++ aulas/modulo4/projeto-astromatch/projeto-astromatch/src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  background-color: lightgray;
+  height: 100vh;
+`
+
+function App() {
+  return (
+    <AppContainer>
+      
+    </AppContainer>
+  );
+}
+
+export default App;
