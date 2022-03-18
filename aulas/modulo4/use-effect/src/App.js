f3cb9eb2b3c215bc684d9926c2a89eead8ee7c3a
Caio Rigotto <caiorigotto@gmail.com>
2022-03-17 23:40:21 -0300

    Não tenho ideia do porque o useEffect não executa

---
+++ aulas/modulo4/use-effect/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { MainContainer } from "./styles";
+import axios from "axios";
+import PokeCard from "./components/PokeCard/PokeCard";
+
+function App (props) {
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
+  
+  useEffect = (() => {
+    getPokemon()
+  },[])
+  
+  const getPokemon = () => {
+    console.log("getting pkm")
+    // axios
+    //   .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    //   .then(response => {
+    //     setPokeList(response.data.results);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  }
+  
+
+  function changePokeName (event) {
+    setPokeName(event.target.value)
+  };
+
+  return (
+    <MainContainer>
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </MainContainer>
+  );
+}
+
+export default App;
