f7b5af2c1c731ff1a36c0caa8c55453342fc457b
BuildTools <caiorigotto@gmail.com>
2022-05-24 23:37:36 -0300

    Exercícios de TS

---
+++ aulas/modulo4/intro-typescript/src/index.ts
@@ -0,0 +1,22 @@
+// Ex1
+
+// function checaTriangulo(a:number, b:number, c:number) :string{
+//     if (a !== b && b !== c) {
+//       return "Escaleno";
+//     } else if (a === b && b === c) {
+//       return "Equilátero";
+//     } else {
+//       return "Isósceles";
+//     }
+//   }
+
+//   console.log(checaTriangulo(12, 13, 14))
+
+// Ex2
+
+function imprimeTresCoresFavoritas() {
+    const cor1 = prompt("Insira sua primeira cor favorita")
+    const cor2 = prompt("Insira sua segunda cor favorita")
+    const cor3 = prompt("Insira sua terceira cor favorita")
+    console.log([cor1, cor2, cor3])
+ }
