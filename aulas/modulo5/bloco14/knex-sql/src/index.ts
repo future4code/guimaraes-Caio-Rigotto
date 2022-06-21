93bdaa60368174fb2f70d4aef21026187a17e870
Caio Rigotto <caiorigotto@gmail.com>
2022-06-21 15:59:10 -0300

    ..

--- aulas/modulo5/bloco14/knex-sql/src/index.ts
+++ aulas/modulo5/bloco14/knex-sql/src/index.ts
@@ -2,3 +2,9 @@ import connection from "./connection";
 import app from "./app";
 import { Response, Request } from "express";
 
+const GetActorById = async (id:string):Promise<any> =>{
+    const result = await connection.raw(`
+        SELECT * FROM actor WHERE id = '${id}'
+    `)
+    return result[0][0]
+}
