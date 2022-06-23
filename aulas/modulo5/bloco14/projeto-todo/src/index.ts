c695e9d211d1a890ab4d5ae9ef276f62211af043
Caio Rigotto <caiorigotto@gmail.com>
2022-06-23 17:59:44 -0300

    Funcionalidade de pegar tarefa por id criada

--- aulas/modulo5/bloco14/projeto-todo/src/index.ts
+++ aulas/modulo5/bloco14/projeto-todo/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import express, { Response, Request } from "express";
 
-import { CreateTask, CreateUser, EditUser } from "./utils/functions"
+import { CreateTask, CreateUser, EditUser, GetTaskById } from "./utils/functions"
 import connection from "./connection";
 
 // TEST CONNECTION
@@ -53,86 +53,111 @@ app.get('/user/:id', async (req: Request, res: Response) => {
     try {
         const id = req.params.id
 
-        if(!id){
+        if (!id) {
             ErrorCode = 400
             throw new Error("Parâmetro id necessário não enviado.")
         }
 
         const users = await connection('toDoListUser')
-            .select('id','nickname')
-            .where({id:id})
+            .select('id', 'nickname')
+            .where({ id: id })
 
-        if(Object.entries(users).length === 0){
+        if (Object.entries(users).length === 0) {
             ErrorCode = 422
             throw new Error("Id não encontrado na database.")
         }
-        
+
         res.status(200).send(users)
     }
-    catch (err:any) {
+    catch (err: any) {
         res.status(ErrorCode).end(err.message)
     }
 })
 
 // EDIT USER BY ID
-app.put('/user/edit/:id', async (req: Request, res: Response)=>{
+app.put('/user/edit/:id', async (req: Request, res: Response) => {
     let ErrorCode = 500
-    try{
+    try {
         const id = Number(req.params.id)
 
-        if(!id){
+        if (!id) {
             ErrorCode = 400
             throw new Error("Parâmetro 'id' não enviado.")
         }
 
         const { name, nickname, email } = req.body
-        
-        if(name === "" || nickname === "" || email === ""){
+
+        if (name === "" || nickname === "" || email === "") {
             ErrorCode = 400
             throw new Error("Parâmetros enviados no body não podem ser vazios.")
         }
 
-        await EditUser(id, name, nickname, email)        
+        await EditUser(id, name, nickname, email)
         res.status(200).send(`Usuário de id:${id} editado com sucesso.`)
     }
-    catch(err:any){
+    catch (err: any) {
         res.status(ErrorCode).end(err.message)
     }
 })
 
 // CREATE TODO TASK
-app.post('/task', async (req: Request, res: Response)=>{
+app.post('/task', async (req: Request, res: Response) => {
     let ErrorCode = 500
-    try{
+    try {
         const { task, desc, status, limitDate, creatorId } = req.body
-        if(!task || !desc || !status || !limitDate || !creatorId){
+        if (!task || !desc || !status || !limitDate || !creatorId) {
             ErrorCode = 400
             throw new Error("Parâmetro necessário não enviado.")
         }
-        if(task === "" || desc === "" || limitDate === "" || creatorId === ""){
+        if (task === "" || desc === "" || limitDate === "" || creatorId === "") {
             ErrorCode = 400
             throw new Error("Parâmetros enviados no body não podem ser vazios")
         }
 
         const date_regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
-        if(!(date_regex.test(limitDate))){
+        if (!(date_regex.test(limitDate))) {
             ErrorCode = 422
             throw new Error("Formato de data errado. DD/MM/YYYY")
         }
 
         const checkUserId = await connection('toDoListUser')
-        .select()
-        .where({id:creatorId})
-        if(Object.entries(checkUserId).length === 0){
+            .select()
+            .where({ id: creatorId })
+        if (Object.entries(checkUserId).length === 0) {
             ErrorCode = 422
             throw new Error("Id do criador não encontrado na database.")
         }
 
-        await CreateTask(task,desc,status,limitDate,creatorId)
+        await CreateTask(task, desc, status, limitDate, creatorId)
 
         res.status(200).send("Tarefa criada com sucesso.")
     }
-    catch(err:any){
+    catch (err: any) {
+        res.status(ErrorCode).end(err.message)
+    }
+})
+
+// GET TODO TASK BY ID
+app.get('/task/:id', async (req: Request, res: Response) => {
+    let ErrorCode = 500
+    try {
+        const id = Number(req.params.id)
+
+        if (!id) {
+            ErrorCode = 400
+            throw new Error("Parâmetro necessário não enviado.")
+        }
+
+        const tasks = await GetTaskById(id)
+
+        if(!tasks){
+            ErrorCode = 422
+            throw new Error("Id de tarefa não encontrado na database.")
+        }
+
+        res.status(200).send(tasks)
+    }
+    catch (err: any) {
         res.status(ErrorCode).end(err.message)
     }
 })
--- aulas/modulo5/bloco14/projeto-todo/src/utils/functions.ts
+++ aulas/modulo5/bloco14/projeto-todo/src/utils/functions.ts
@@ -24,22 +24,54 @@ export const EditUser = async (id: number,
         .where({ id: id })
 }
 
-export const CreateTask = async (task:string, 
-    desc:string, 
-    status:string, 
-    limitDate:string, 
-    creatorId:number): Promise<any> => {
-        
-        const dateSplit = limitDate.split('/')
+export const CreateTask = async (task: string,
+    desc: string,
+    status: string,
+    limitDate: string,
+    creatorId: number): Promise<any> => {
 
-        const sqlDate = `${dateSplit[2]}-${dateSplit[1]}-${dateSplit[0]}`
+    const dateSplit = limitDate.split('/')
 
-        await connection('toDoList')
+    const sqlDate = `${dateSplit[2]}-${dateSplit[1]}-${dateSplit[0]}`
+
+    await connection('toDoList')
         .insert({
-            title:task,
-            description:desc,
+            title: task,
+            description: desc,
             status,
-            limit_date:sqlDate,
-            creator_user_id:creatorId
+            limit_date: sqlDate,
+            creator_user_id: creatorId
         })
+}
+
+export const GetTaskById = async (id: number): Promise<any> => {
+    const tasks = await connection('toDoList')
+        .select()
+        .where({ id: id })
+
+    if (Object.entries(tasks).length === 0) {
+        return null
     }
+
+    const task = tasks[0]
+
+    const creatorName = await connection('toDoListUser')
+        .select()
+        .where({ id: task.creator_user_id })
+
+    const dateSplit = task.limit_date.toISOString().split('-')
+
+    const limitDate = `${dateSplit[2].slice(0, 2)}/${dateSplit[1]}/${dateSplit[0]}`
+
+    const taskInfo = [{
+        taskId: task.id,
+        title: task.title,
+        description: task.description,
+        limitDate: limitDate,
+        status: task.status,
+        creatorUserId: task.creator_user_id,
+        creatorUserNickname: creatorName[0].nickname
+    }]
+
+    return taskInfo
+}
