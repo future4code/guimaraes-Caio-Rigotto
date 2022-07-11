f8fe22ce7f3805abf343e99efd06cf3fab2f19d6
Caio Rigotto <caiorigotto@gmail.com>
2022-07-10 23:37:56 -0300

    Funcionalidades cadastro de prod/busca por prod/registro de compra/busca de compra por id do usuário

--- aulas/modulo5/bloco14/projeto-labecommerce/src/utils/functions.ts
+++ aulas/modulo5/bloco14/projeto-labecommerce/src/utils/functions.ts
@@ -1,5 +1,5 @@
 import connection from "../connection"
-import { user } from "./types"
+import { user, prod, purchase } from "./types"
 
 export const CreateUser = async (user: user) => {
 
@@ -8,5 +8,43 @@ export const CreateUser = async (user: user) => {
             name: user.name,
             email: user.email,
             password: user.password
+        })
+}
+
+export const CreateProduct = async (prod: prod) => {
+
+    await connection('labecommerce_products')
+        .insert({
+            name: prod.name,
+            price: prod.price,
+            img_url: prod.img_url
+        })
+}
+
+export const GetProductsByUserId = async (userId: number) => {
+    const products = await connection('labecommerce_users')
+        .join('labecommerce_purchases',
+            'labecommerce_purchases.user_id',
+            connection.raw('?', [`${userId}`])
+        )
+        .select('name', 'user_id', 'product_id', 'quantity', 'total_price')
+
+    return products
+}
+
+export const RegisterPurchase = async (purchase: purchase) => {
+
+    const prod = await connection('labecommerce_products')
+        .select('price')
+        .where({ 'id': `${purchase.prodId}` })
+    const price = Number(prod[0].price)
+    const total_price = Number(price * purchase.quantity)
+
+    await connection('labecommerce_purchases')
+        .insert({
+            user_id: purchase.userId,
+            product_id: purchase.prodId,
+            quantity: purchase.quantity,
+            total_price
         })
 }
--- aulas/modulo5/bloco14/projeto-labecommerce/src/utils/types.ts
+++ aulas/modulo5/bloco14/projeto-labecommerce/src/utils/types.ts
@@ -3,3 +3,15 @@ export type user = {
     email: string,
     password: string
 }
+
+export type prod = {
+    name: string,
+    price: number,
+    img_url: string
+}
+
+export type purchase = {
+    userId: number,
+    prodId: number,
+    quantity: number
+}
