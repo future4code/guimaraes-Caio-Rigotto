202e356961000e8f01d64e9c03bab0042454ff0b
Caio Rigotto <caiorigotto@gmail.com>
2022-07-12 23:49:47 -0300

    Ex 1

--- aulas/modulo5/bloco14/classes-encapsulamento/src/index.ts
+++ aulas/modulo5/bloco14/classes-encapsulamento/src/index.ts
@@ -1,16 +1,28 @@
-import app from "./app";
-import express, { Response, Request } from "express";
+// 1.a. O constructor é um método de classe que permite o acesso às informações de um objeto.
+// O chamamos com a função constructor(){}
 
-import { CreateTask, CreateUser, EditUser, GetAllUsers, GetTaskById, GetTaskByUserId, SearchForUser, AssignUserTask, GetTaskAssignedUser } from "./utils/functions"
-import connection from "./connection";
+// 1.b. O constructor foi chamado apenas uma vez.
 
-// TEST CONNECTION
-app.get('/ping', (req: Request, res: Response) => {
-    try {
-        res.status(200).send("Pong")
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    // private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
     }
-    catch (err) {
-        res.status(500).end("Ocorreu algo de errado.")
-    }
-})
+  
+  }
 
+  const userTest:UserAccount = new UserAccount("11111111111", "Joaquim", 22)
+
+  // 1.c. Precisamos criar métodos dentro da classe que acessam estas informações.
