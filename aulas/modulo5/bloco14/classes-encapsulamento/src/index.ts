f1138a7043dfd6901024b8d8f128460df4634904
Caio Rigotto <caiorigotto@gmail.com>
2022-07-13 00:02:35 -0300

    Ex 2

--- aulas/modulo5/bloco14/classes-encapsulamento/src/index.ts
+++ aulas/modulo5/bloco14/classes-encapsulamento/src/index.ts
@@ -8,21 +8,42 @@ class UserAccount {
     private name: string;
     private age: number;
     private balance: number = 0;
-    // private transactions: Transaction[] = [];
-  
+    private transactions: Transaction[] = [];
+
     constructor(
-       cpf: string,
-       name: string,
-       age: number,
+        cpf: string,
+        name: string,
+        age: number,
     ) {
-       console.log("Chamando o construtor da classe UserAccount")
-       this.cpf = cpf;
-       this.name = name;
-       this.age = age;
+        console.log("Chamando o construtor da classe UserAccount")
+        this.cpf = cpf;
+        this.name = name;
+        this.age = age;
     }
-  
-  }
+}
 
-  const userTest:UserAccount = new UserAccount("11111111111", "Joaquim", 22)
+const userTest: UserAccount = new UserAccount("11111111111", "Joaquim", 22)
 
-  // 1.c. Precisamos criar métodos dentro da classe que acessam estas informações.
+// 1.c. Precisamos criar métodos dentro da classe que acessam estas informações.
+
+// 2.
+class Transaction {
+    private description: string;
+    private value: number;
+    private date: string
+    
+    constructor(description: string,
+        value: number,
+        date: string) {
+        this.description = description;
+        this.value = value;
+        this.date = date;
+    }
+    public getDescription = () =>{
+        console.log (this.description)
+    }
+}
+
+const transaction1 = new Transaction("descrição", 12, "data")
+
+transaction1.getDescription()
