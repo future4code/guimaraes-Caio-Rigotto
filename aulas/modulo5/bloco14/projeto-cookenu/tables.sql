8d95531cf9ba855d47683ae33cd36abcd4e8e741
Caio Rigotto <caiorigotto@gmail.com>
2022-08-25 00:47:56 -0300

    Funcionalidade de registro

---
+++ aulas/modulo5/bloco14/projeto-cookenu/tables.sql
@@ -0,0 +1,26 @@
+-- Active: 1660620348611@@35.226.146.116@3306@guimaraes-4211162-caio-rigotto
+
+CREATE TABLE IF NOT EXISTS Cookenu_users (
+    id VARCHAR(64) PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Cookenu_recipes (
+    id VARCHAR(64) PRIMARY KEY,
+    title VARCHAR(64) NOT NULL,
+    description VARCHAR(1024) NOT NULL,
+    created_at DATE NOT NULL,
+    author_id VARCHAR(64) NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Cookenu_users(id)
+);
+
+CREATE TABLE IF NOT EXISTS Cookenu_assignees (
+    recipe_id VARCHAR(64) NOT NULL,
+    assignee_id VARCHAR(64) NOT NULL,
+    PRIMARY KEY (recipe_id, assignee_id),
+    FOREIGN KEY (recipe_id) REFERENCES Cookenu_recipes(id),
+    FOREIGN KEY (assignee_id) REFERENCES Cookenu_users(id)
+);
+
