9c14d3d556c038beb435a141dad945418ff0f810
Caio Rigotto <caiorigotto@gmail.com>
2022-10-03 18:34:16 -0300

    ...

--- aulas/cases/rodada-cases-bloco-2/promobit/src/pages/home.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { HomeContainer } from "./styledHome";
-
-export default class Home extends React.Component{
-    render(){
-        return <HomeContainer> HOME</HomeContainer >
-    }
-}
